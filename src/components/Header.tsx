4aef0c1ae8ad83ca6116a76621e03c3ca5567a21
goodydev44 <goodyewoma44@gmail.com>
2024-10-11 22:04:46 +0100

    bg-img added; bg-color white; comment box added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,13 +4,13 @@ import Logo from "./Logo";
 const Header: React.FC = () => {
   return (
     <header className="contain-style1 flex mobile:flex-col mobile:justify-center
-            md:flex-row md:justify-evenly gap-2">
+            md:flex-row md:justify-between gap-2">
       <div className="manageLogo">
         <Logo />
         <h3 className="manage text-3xl font-bold">manage</h3>
       </div>
       <nav>
-        <ul className="grid mobile:grid-cols-3 md2:grid-cols-4 md3:grid-cols-5 xs:gap-5000 font-[500]">
+        <ul className="grid mobile:grid-cols-3 md2:grid-cols-4 md3:grid-cols-5 font-[500]">
           <li className="list-styles1">Pricing</li>
           <li className="list-styles1">Product</li>
           <li className="list-styles1">About</li>
@@ -18,7 +18,7 @@ const Header: React.FC = () => {
           <li className="list-styles1">Community</li>
         </ul>
       </nav>
-      <button className="bt-style1 max-h-10">
+      <button className="bt-style1 max-h-10 xs:min-w-60 md:min-w-0 mx-auto">
         Get Started
       </button>
     </header>
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -4,10 +4,12 @@ import Section1 from "./Section1";
 import Section2 from "./Section2";
 import Section3 from "./Section3";
 import Footer from "./Footer";
+import Img from '../assets/Images/bg-tablet-pattern.svg'
 
 const Main: React.FC = () => {
   return (
-    <div className="Main-styles">
+    <div className="mobile:mt-5 xs:mt-14 bg-no-repeat bg-right-top"
+        style={ {backgroundImage: `url(${Img})`} }>
       <Header />
       <Section1 />
       <Section2 />
