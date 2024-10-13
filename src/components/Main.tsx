60c1cc44287aeeeb655bc7c21e442dce8b1f5139
goodydev44 <goodyewoma44@gmail.com>
2024-10-13 16:22:03 +0100

    bg-img config

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -4,18 +4,16 @@ import Section1 from "./Section1";
 import Section2 from "./Section2";
 import Section3 from "./Section3";
 import Footer from "./Footer";
-import Img from '../assets/Images/bg-tablet-pattern.svg'
 
 const Main: React.FC = () => {
   return (
-    <div className="mobile:mt-5 xs:mt-14 bg-no-repeat bg-right-top"
-        style={ {backgroundImage: `url(${Img})`} }>
+    <main className="mobile:mt-5 xs:mt-14">
       <Header />
       <Section1 />
       <Section2 />
       <Section3 />
       <Footer />
-    </div>
+    </main>
   );
 };
 
